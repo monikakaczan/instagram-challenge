fd9ed16b84420ee077505b0cbd3c009c2a52beac
Student <kaczanowskamonika@gmail.com>
2019-07-21 20:41:10 +0100

    posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,36 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!
   def new
+    @post = Post.new
+
   end
 
   def index
+    @posts = Post.all.reverse
+
   end
 
   def show
+    @post = Post.find(params[:id])
+
   end
+
+  def create
+    @post = Post.create(post_params.merge(user_id: current_user.id))
+    if @post.save
+      flash[:success] = "Your post has been created successfully"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Please try again"
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+  
 end
