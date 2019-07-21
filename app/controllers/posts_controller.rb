adb33e5a24262406e95bacedf127443fa1585fa2
Student <kaczanowskamonika@gmail.com>
2019-07-21 18:47:51 +0100

    half broken so far

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,33 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.create(post_params)
+    # .merge(user_id: current_user.id))
+    if @post.save
+      flash[:success] = "Post created successfully"
+      redirect_to posts_path(@post)
+    else
+      flash.now[:alert]= "Oops, please try again."
+      render :new
+    end
+  end
+
+  private
+
+
+  def post_params
+    params.require(:post).permit(:image, :description)
+  end
+
 end
